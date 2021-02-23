1d1833d8f8240ce12c4dd55f240e27bfc29aa7d8
katerynakofanova <katerynakofanova@gmail.com>
2021-02-23 21:22:55 +0200

    tasks 8.8-8.12 done

---
+++ chapter8/library-frontend/src/components/Authors.js
@@ -0,0 +1,87 @@
+  
+import React, {useState} from 'react'
+import {EDIT_AUTHOR, ALL_AUTHORS} from '../queries'
+import { useQuery, useMutation } from '@apollo/client'
+
+
+const Authors = (props) => {
+  const result = useQuery(ALL_AUTHORS)
+  const [name, setName] = useState('')
+  const [born, setBorn] = useState('')
+
+  const [editAuthor] = useMutation(EDIT_AUTHOR, { 
+    onError: (error) => {
+      console.log(error.graphQLErrors[0].message)
+    }
+  })
+
+  const submit = async (event) => {
+    event.preventDefault()
+    editAuthor({
+      variables:{
+        name,
+        setBornTo: Number(born)
+      }
+    })
+    setName('')
+    setBorn('')
+  }
+
+  if (!props.show) {
+    return null
+  }else{
+    if(result.loading){
+      return <div>loading...</div>
+    }
+    if(result.data){
+      const authors = result.data.allAuthors
+    return (
+      <div>
+        <h2>authors</h2>
+        <table>
+          <tbody>
+            <tr>
+              <th></th>
+              <th>
+                born
+              </th>
+              <th>
+                books
+              </th>
+            </tr>
+            {authors.map(a =>
+              <tr key={a.name}>
+                <td>{a.name}</td>
+                <td>{a.born}</td>
+                <td>{a.bookCount}</td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+        <h3>Set birthyear</h3>
+              <form onSubmit={submit}>
+                <div>
+                  name: <select
+                  value={name}
+                  onChange={({ target }) => setName(target.value)}>
+                    {authors.map(a => <option value={a.name} key={a.name}>{a.name}</option>)}
+                    </select>
+                </div>
+                <div>
+                  born: <input 
+                  type='number'
+                  value={born}
+                  required={true}
+                  onChange={({ target }) => setBorn(target.value)}/>
+                </div>
+                <button type='submit'>update author</button>
+              </form>
+  
+      </div>
+    )
+    }
+  }
+}
+
+
+export default Authors
